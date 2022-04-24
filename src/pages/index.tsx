884c725f999dcc79949892d6a1f8d416d24dd846
rafaeelnunes <rafaelnfsq@gmail.com>
2022-04-24 11:40:07 -0300

    initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import Instructor from "./instructor";
+import SignIn from "./signIn";
+import SignUp from "./signUp";
+import Subjects from "./subjects";
+
+export { Instructor, SignIn, SignUp, Subjects };
---
+++ src/services/api.tsx
@@ -0,0 +1,2 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:5000/";
